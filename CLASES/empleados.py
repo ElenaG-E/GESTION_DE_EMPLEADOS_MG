b85231cff41c225be4d0c1a4a841175ec2266f3e
Elena <ezzargi@gmail.com>
2024-10-22 16:12:05 -0300

    #

--- CLASES/empleados.py
+++ CLASES/empleados.py
@@ -1,10 +1,14 @@
+from cryptography.fernet import Fernet
 import re
+
 class Empleados:
-    def __init__(self, id_rut: str, nombre: str, apepat: str, apemat: str, fecha_nac, fecha_contrato, sueldo, correo: str, telefono: str, direccion, id_rol, id_tipo: str, nom_usuario, password):
+
+    clave = Fernet.generate_key()
+    cipher_suite = Fernet(clave)
+
+    def __init__(self, id_rut, nombre, fecha_nac, fecha_contrato, sueldo, correo, telefono, direccion, id_rol, id_tipo, nom_usuario, password):
         self.id_rut = id_rut
         self.nombre = nombre
-        self.apepat = apepat
-        self.apemat = apemat
         self.fecha_nac = fecha_nac
         self.fecha_contrato = fecha_contrato
         self.sueldo = sueldo
@@ -16,5 +20,21 @@ class Empleados:
         self.nom_usuario = nom_usuario
         self.password = password
     
+    def Encriptar_clave(self):
+        if self.password:
+            return self.cipher_suite.decrypt(self.password.encode().decode())
+        return ""
+        
+    def Validar_datos(self):
+        self.nombre = input("Ingresa el nombre del empleado: ")
+        self.correo = input("Ingresa el correo: ")
+        self.fecha_nac = input("Ingresa fecha_nacimiento: ")
+        self.telefono= input("Ingresa el telefono: ")
+    
     def __str__(self):
         return f"Empleado: {self.nombre} - Correo: {self.correo} - Fecha_Nac: {self.fecha_nac} - Telefono: {self.telefono}"
+
+    def Encriptar_clave(self):
+        if self.password:
+            return self.cipher_suite.decrypt(self.password.encode().decode())
+        return ""
