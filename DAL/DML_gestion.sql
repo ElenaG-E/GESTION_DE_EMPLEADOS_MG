54e37a9fcf072d9b5f23a268b31ed823d70576f2
Elena <ezzargi@gmail.com>
2024-10-29 15:38:16 -0300

    #

---
+++ DAL/DML_gestion.sql
@@ -0,0 +1,7 @@
+INSERT INTO `clientes` (`ID_RUT`, `NOMBRE`, `FECHA_NAC`, `FECHA_CONTRATO`, `SALARIO`, `CORREO`, `TELEFONO`, `DIRECC`, `ID_ROL`, `ID_TIPO`, `NOM_USUARIO`, `PASSWORD`) VALUES
+(174563829, 'Ana López', '1990-04-17', '2021-03-10', 1800000, 'ana.l@medsupply.com', '+56909876543', 'Avenida Siempre Viva 742', 3, 3, 'analopez', 'password9101'),
+(188276534, 'Miguel Torres', '1983-11-30', '2018-12-01', 2200000, 'miguel.t@medsupply.com', '+56911223344', 'Pasaje los Sauces 45', 1, 2, 'migueltorres', 'password1122'),
+(195186911, 'María Gutierrez', '1985-07-23', '2020-01-15', 1500000, 'maria.g@medsupply.com', '+56912345678', 'Villasur Marina 15', 1, 2, 'mariaguti', 'password1234'),
+(202392017, 'José Martínez', '1978-09-12', '2019-06-20', 2000000, 'jose.m@medsupply.com', '+56987654321', 'Calle Falsa 123', 2, 1, 'josemartinez', 'password5678'),
+(206541237, 'Laura Fernández', '1995-02-28', '2020-07-23', 1700000, 'laura.f@medsupply.com', '+56922334455', 'Camino Real 456', 2, 3, 'laurafernandez', 'password1314');
+
