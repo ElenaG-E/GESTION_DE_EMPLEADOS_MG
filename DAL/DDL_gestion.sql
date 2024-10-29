3ccbb62618c72f220aa9ae00c2e37d68c356b731
Elena <ezzargi@gmail.com>
2024-10-29 15:48:23 -0300

    #

--- DAL/DDL_gestion.sql
+++ DAL/DDL_gestion.sql
@@ -1,43 +1,17 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 29-10-2024 a las 19:36:16
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `gestion_empleados_mg`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `asignacion`
---
-
 CREATE TABLE `asignacion` (
   `ID_ASIG` int(11) NOT NULL,
   `ID_DEPTO` int(11) NOT NULL,
-  `ID_RUT` int(11) NOT NULL
+  `ID_RUT` int(11) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `clientes`
---
 
 CREATE TABLE `clientes` (
   `ID_RUT` int(11) NOT NULL,
@@ -54,22 +28,15 @@ CREATE TABLE `clientes` (
   `PASSWORD` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `clientes`
---
 
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `departamento`
 --
 
 CREATE TABLE `departamento` (
   `ID_DEPTO` int(11) NOT NULL,
   `NOMBRE` varchar(60) NOT NULL,
-  `ID_RUT` int(11) NOT NULL
+  `ID_RUT` int(11) NOT NULL,
+  PRIMARY KEY (ID_DEPTO),
+  CONSTRAINT FOREIGN KEY (ID_RUT) REFERENCES EMPLEADOS(ID_RUT)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
